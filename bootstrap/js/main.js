f6fe41d483b77b4e1f460227e498fdc35e92a4c6
Peter Werner <pewerner@comcast.net>
2014-11-24 15:56:34 -0500

    Fixed Bugs

---
+++ bootstrap/js/main.js
@@ -0,0 +1,26 @@
+// JavaScript Document
+	$(function () {
+		
+		
+		
+	//alert(navigator.userAgent.match(/iPad/i));
+      // Slideshow 1
+      $("#slider1").responsiveSlides({
+        maxwidth: 800,
+        speed: 800,
+		pause: true,
+		 
+      });
+
+    });
+   
+	$(document).ready(function() {
+	
+
+	
+
+     });
+	 
+	
+
+   
---
+++ join.md
@@ -0,0 +1,6 @@
+---
+layout: page
+title: "Join"
+description: ""
+---
+{% include JB/setup %}
