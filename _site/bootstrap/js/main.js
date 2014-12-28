3a468e7988ac3832d2a86f8f0705e5c2f9933623
Peter Werner <pewerner@comcast.net>
2014-12-28 10:52:48 -0500

    Deleted some stuff

---
+++ _site/bootstrap/js/main.js
@@ -0,0 +1,30 @@
+// JavaScript Document
+	$(function () {
+		
+		
+		
+	//alert(navigator.userAgent.match(/iPad/i));
+      // Slideshow 1
+      $("#slider1").responsiveSlides({
+        maxwidth: 800,
+        speed: 800,
+		pause: true,
+		 
+      });
+
+    });
+   
+	$(document).ready(function() {
+	
+
+
+!function(d,s,id){
+    
+    var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?"http":"https";if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document, "script", "twitter-wjs");
+	
+
+     });
+	 
+	
+
+   
