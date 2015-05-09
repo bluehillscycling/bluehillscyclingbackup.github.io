954347d527f1d4b690c4a1e77daa5f1c6d3be5c7
Peter Werner <pedro@bluehillscycling.com>
2015-05-09 15:46:52 -0400

    changed post layout

--- bootstrap/js/main.js
+++ bootstrap/js/main.js
@@ -1,9 +1,6 @@
 // JavaScript Document
 	$(function () {
-		
-		
-		
-	//alert(navigator.userAgent.match(/iPad/i));
+
       // Slideshow 1
       $("#slider1").responsiveSlides({
         maxwidth: 800,
